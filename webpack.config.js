c0447e2eb4539168809a531cc36e3be62be9c808
willou <walid.boubguira@gmail.com>
2020-04-08 11:00:34 -0400

    init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "app"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", "jsx"],
+  },
+  devServer: {
+    historyApiFallback: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
